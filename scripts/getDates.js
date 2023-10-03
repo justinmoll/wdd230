37d99e5a14f610ca2cc7d0073a4638a1fc4d41aa
JustinMoll <justinmoll785@gmail.com>
2023-10-03 09:51:14 -0600

    added a hamburger menu and worked on dark mode

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -13,3 +13,30 @@ let lastModifiedElement = document.querySelector('#last-modified');
 if (lastModifiedElement) {
     lastModifiedElement.textContent =  `Last Modified: ${lastModified}`
 };
+
+// Adds Hamburger Menu 
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+// Add a dark mode button
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const li = document.querySelector("#hyperlink");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#293241";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+        // li.style.color = "#fff";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
